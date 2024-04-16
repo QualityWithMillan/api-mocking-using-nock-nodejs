6ea562e0b115cbe47e0ff0692032a0f9878cdd48
#QualityWithMillan <120608534+QualityWithMillan@users.noreply.github.com>
2024-04-15 21:52:33 -0700

    Update util.js
    Updated expect from chai and exports

--- util/util.js
+++ util/util.js
@@ -6,20 +6,25 @@
  */
 
 /**
- * All Helper functions 
+ * Helper functions
  */
-const https = require('https');
+
+import https from 'https';
+import { expect } from 'chai';
 
 // Helper function to fetch data from URL
-async function fetchData(url) {
+export default async function fetchData(url) {
     return new Promise((resolve, reject) => {
       https.get(url, (res) => {
         let data = '';
         res.on('data', (chunk) => (data += chunk));
         res.on('end', () => resolve(JSON.parse(data)));
+        console.log("Response STATUS: ",res.statusCode);
+        /** assert using chai response code to be 200 **/
+        expect(res.statusCode).to.equal(200)
       }).on('error', (error) => reject(error));
     });
   }
 
 // CommonJS syntax
-module.exports = { fetchData };  
+// module.exports = { fetchData };  
